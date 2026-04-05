87909a5567985a59e27071252027150e7a30da63
pmnw1234 <phoomyatnoewai8489@gmail.com>
2026-04-05 18:18:11 +0630

    register

--- useraccount/views.py
+++ useraccount/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
+from .forms import UserRegistrationForm
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+
+            if User.objects.filter(username=username).exists():
+                form.add_error('username', 'Username already exists')
+                return render(request, 'planner/register.html', {'form': form})
+
+
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password
+            )
+
+            profile = form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
