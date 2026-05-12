341966ebd17fefb678e88fc59d36cf26c0f0b703
Khin Than Thar <115642358+rochellelovegood@users.noreply.github.com>
2026-05-12 15:14:41 +0630

    add filter and some changes for post card

--- feedview/models.py
+++ feedview/models.py
@@ -1,25 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Post(models.Model):
     POST_TYPE_CHOICES = [
         ('study', 'Study Partner'),
         ('skill', 'Skill Swap'),
+    ]
+    
+    POST_CATEGORY_CHOICES = [
+        ('skill_swap', 'Skill Swap'),
+        ('study_partner', 'Study Partner'),
+    ]
+    
+    POST_TOPIC_CHOICES = [
+        ('language', 'Language'),
+        ('tech', 'Technology'),
+        ('science', 'Science'),
+        ('art', 'Art'),
+        ('business', 'Business'),
+        ('general', 'General'),
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True)
     image = models.ImageField(upload_to='posts/', blank=True, null=True)
     video = models.FileField(upload_to='posts/videos/', blank=True, null=True)
-
-    post_type = models.CharField(
-    max_length=10,
-    choices=POST_TYPE_CHOICES,
-    default='study'  
     
-)
-
+    post_type = models.CharField(max_length=10, choices=POST_TYPE_CHOICES, default='study')
+    category = models.CharField(max_length=20, choices=POST_CATEGORY_CHOICES, default='skill_swap')
+    topic = models.CharField(max_length=20, choices=POST_TOPIC_CHOICES, default='general')
+    hashtags = models.CharField(max_length=200, blank=True, help_text="Comma separated: #tech,#python")
+    
     created_at = models.DateTimeField(auto_now_add=True)
 
     def total_likes(self):
@@ -47,18 +58,18 @@ class Interested(models.Model):
     class Meta:
         unique_together = ['user', 'post']
 
+
 class MatchRequest(models.Model):
     sender = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='match_sent_requests'   # ✅ changed
+        related_name='match_sent_requests'
     )
     receiver = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='match_received_requests'  # ✅ changed
+        related_name='match_received_requests'
     )
-
     status = models.CharField(
         max_length=20,
         choices=[
@@ -68,10 +79,7 @@ class MatchRequest(models.Model):
         ],
         default='pending'
     )
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = ['sender', 'receiver']
-
-# Create your models here.
