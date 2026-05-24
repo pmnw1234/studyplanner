70178711e87b4d5e9fa39956242bbc07c6026436
pmnw1234 <phoomyatnoewai8489@gmail.com>
2026-05-24 11:30:58 +0630

    Interested send note and comment replies

--- feedview/models.py
+++ feedview/models.py
@@ -30,8 +30,9 @@ class Post(models.Model):
     category = models.CharField(max_length=20, choices=POST_CATEGORY_CHOICES, default='skill_swap')
     topic = models.CharField(max_length=20, choices=POST_TOPIC_CHOICES, default='general')
     hashtags = models.CharField(max_length=200, blank=True, help_text="Comma separated: #tech,#python")
-    
+    updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_edited = models.BooleanField(default=False)
 
     def total_likes(self):
         return self.likes.count()
@@ -83,3 +84,56 @@ class MatchRequest(models.Model):
 
     class Meta:
         unique_together = ['sender', 'receiver']
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+
+    class Meta:
+        ordering = ['created_at'] # Oldest comments/replies show first
+
+    def __str__(self):
+        return f"{self.user.username}: {self.content[:20]}"
+
+class Notification(models.Model):
+    NOTIFICATION_TYPES = [
+        ('like', 'Like'),
+        ('comment', 'Comment'),
+        ('interest', 'Interest'),
+    ]
+
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='sent_notifications'
+    )
+
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='received_notifications'
+    )
+
+    post = models.ForeignKey(   # ADD THIS
+        Post,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    notification_type = models.CharField(
+        max_length=20,
+        choices=NOTIFICATION_TYPES
+    )
+
+    message = models.CharField(max_length=255)
+    note = models.TextField(blank=True, null=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
